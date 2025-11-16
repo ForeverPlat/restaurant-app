4c5cabb1b5917d18196998920ce92fc2f6750ff2
foreverplat <luqman.o.ajani@gmail.com>
2025-11-15 23:41:41 -0700

    description based model working (going to experiment with hybrid - description and tag based)

--- backend/recommender/recommend.py
+++ backend/recommender/recommend.py
@@ -26,7 +26,8 @@ def recommend_by_restaurant(restaurant, top_n=5):
     restaurants = df[df["name"].str.contains(restaurant["name"], regex=True)].index
 
     if restaurants.empty:
-        return recommend_by_tag(restaurant["tags"])
+        # return recommend_by_description(restaurant["description"])
+        return recommend_by_tags(restaurant["tags"])
 
     index = restaurants[0]
     ref_embedding = embeddings[index] # get the embedding of restaurants[0]
@@ -48,8 +49,30 @@ def recommend_by_restaurant(restaurant, top_n=5):
     print("rec by rest end")
     return results
 
+def recommend_by_description(description, top_n=5):
+    print("rec by desc: " + description)
 
-def recommend_by_tag(tags, top_n=5):
+    description = description.lower()
+
+    dVec = model.encode(description, convert_to_tensor=True)
+
+    sim_scores = util.pytorch_cos_sim(dVec, embeddings)[0]
+
+    top_results = sim_scores.topk(top_n)
+    
+    matches = []
+
+    # return [df.iloc[i.item()]["name"] for i in top_results.indices]
+
+    for score, indx in zip(top_results.values, top_results.indices):
+        indx = indx.item()
+
+        matches.append(df.iloc[indx]["name"])
+    
+    return matches
+
+
+def recommend_by_tags(tags, top_n=5):
     print("rec by tag: " + tags)
 
     tags = tags.replace(",", " ").lower()
@@ -75,7 +98,8 @@ restaurant = {
     "cuisine": "Fast Food",
     "price_range": "$",
     "rating": "4.0",
-    "tags": "burgers, perogies, fries, fast food, student"
+    "tags": "burgers, perogies, fries, fast food, student",
+    "description": "Campus-adjacent grease haven slinging juicy smash burgers and golden fries alongside Alberta-style cheddar-perogie poutines that drip with gravy. Cheap combo meals and $5 milkshakes keep university students fueled through late-night cram sessions."
 }
 
 print(recommend_by_restaurant(restaurant))
