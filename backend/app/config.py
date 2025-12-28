5e14e63e9101f25eb76114496bba8b7bb7321c5d
foreverplat <luqman.o.ajani@gmail.com>
2025-12-28 15:59:29 -0700

    refactor how env vars are loaded

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,9 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_PLACES_API_KEY")
+
+if not GOOGLE_API_KEY:
+    raise RuntimeError("GOOGLE_PLACES_API_KEY is not set")
